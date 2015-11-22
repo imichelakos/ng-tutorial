4620218212fbf8ee58534414a8eda41b75b72837
Ioannis Michelakos <imichelakos@gmail.com>
2015-11-23 00:38:18 +0200

    Step 05

--- js/controllers.js
+++ js/controllers.js
@@ -6,26 +6,20 @@
   // body...Wrapping your Javascript in a closure is a good habit!
 
   var phonecatApp = angular.module('phonecatApp',[]);
-  // το πρώτο στοιχείο που συμπίπτει με το όνομα της παραμέτρου είναι το όνομα της εφαρμογής
-  // το δεύτερο στοιχείο ([]), αφορά τις απαιτούμενες βιβλιοθήκες αν υπάρχουν
+  // We'll use Angular's $http service in our controller to make an HTTP request to your web server to fetch the data in the app/phones/phones.json file
 
-  phonecatApp.controller('PhoneListCtrl',function () { /*στο σημείο αυτό μπορούμε να περάσουμε ως παράμετρο την $scope -- 2η μέθοδος -- */
-    // body...
-    /* Ορισμός του πίνακα phones που περιέχει τα δεδομένα*/
-    this.phones = [
-      {'name':'Nexus S',
-       'snippet':'Fast just got faster with Nexus S.',
-       'age':1},
-      {'name':'Motorola XOOM™ with Wi-Fi',
-       'snippet':'The Next, Next Generation tablet.',
-       'age':2},
-      {'name':'Motorola XOOM',
-       'snippet':'The Next, Next Generation tablet.',
-       'age':3}
-    ];
-    // added an age property to each phone record. This property is used to order phones by age
+  phonecatApp.controller('PhoneListCtrl',['$http',function ($http) {
+    // $http returns a Promise, so success gets the data
+    var vm=this; //Δεν μπορούμε μέσα στο success να χρησιμοποιήσουμε το this.
+    $http.get('phones/phones.json')
+      .success(
+        function (data) {
+          // body...
+          vm.phones = data;
+        }
+      );
 
     // sets the default value of orderProp to age
     this.orderProp='age';
-  });
+  }]);
 })();
