4d94df3e36712799455781e84b8c83f6e7787e4e
Ioannis Michelakos <imichelakos@gmail.com>
2015-11-23 00:07:31 +0200

    Step 04

--- js/controllers.js
+++ js/controllers.js
@@ -14,11 +14,18 @@
     /* Ορισμός του πίνακα phones που περιέχει τα δεδομένα*/
     this.phones = [
       {'name':'Nexus S',
-       'snippet':'Fast just got faster with Nexus S.'},
+       'snippet':'Fast just got faster with Nexus S.',
+       'age':1},
       {'name':'Motorola XOOM™ with Wi-Fi',
-       'snippet':'The Next, Next Generation tablet.'},
+       'snippet':'The Next, Next Generation tablet.',
+       'age':2},
       {'name':'Motorola XOOM',
-       'snippet':'The Next, Next Generation tablet.'}
+       'snippet':'The Next, Next Generation tablet.',
+       'age':3}
     ];
+    // added an age property to each phone record. This property is used to order phones by age
+
+    // sets the default value of orderProp to age
+    this.orderProp='age';
   });
 })();
